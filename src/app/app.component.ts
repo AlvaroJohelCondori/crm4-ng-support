9a190182d27bd031f001df977b573ce50011c415
Alvaro Johel Condori <alcondori@hansa.com.bo>
2025-03-30 15:12:34 -0400

    configuración del layout uso de secciones en el menu lateral.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { CommonModule } from '@angular/common';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-root',
@@ -20,13 +21,13 @@ import { CommonModule } from '@angular/common';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatExpansionModule,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'HBM Soporte';
   isMenuOpen = true;
 
   toggleMenu() {
