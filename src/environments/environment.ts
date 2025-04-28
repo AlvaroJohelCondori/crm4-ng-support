a7ce54eeb65bf28d437c745ee5295306ee81d3b3
Alvaro Johel Condori <alcondori@hansa.com.bo>
2025-04-28 14:22:20 -0400

    Update environment.ts para COFAR 365 PRD

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   configurarFirebase: {
+    // COFAR PRD
     apiKey: 'AIzaSyCeXMOS2g8ZTmWJy_fFj7ZwzYd7bBCTchA',
     authDomain: 'hbm-cofar.firebaseapp.com',
     databaseURL: 'https://hbm-cofar-default-rtdb.firebaseio.com',
@@ -9,5 +10,15 @@ export const environment = {
     messagingSenderId: '14357240146',
     appId: '1:14357240146:web:8f099a9fe171895998828b',
     measurementId: 'G-VL0REQNLD0',
+
+    // LAFAR PRD
+    // apiKey: 'AIzaSyATCCFK1D3x7sdkE2qSlPFGv8W30Gj8TDk',
+    // authDomain: 'hbm-lafar.firebaseapp.com',
+    // databaseURL: 'https://hbm-lafar-default-rtdb.firebaseio.com',
+    // projectId: 'hbm-lafar',
+    // storageBucket: 'hbm-lafar.firebasestorage.app',
+    // messagingSenderId: '596932185449',
+    // appId: '1:596932185449:web:714c2ecc95a46d9d8101f8',
+    // measurementId: 'G-3WFGH0M5Q2',
   },
 };
